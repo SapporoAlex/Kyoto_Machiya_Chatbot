11c802cd2e47966e0c058c88989834622fcfd561
Alexander McKinley <alexanderfromaustralia@gmail.com>
2025-03-01 13:37:47 +0900

    Create chatBot.js

---
+++ static/chatBot.js
@@ -0,0 +1,4 @@
+document.getElementById("chatbot-button").addEventListener("click", function () {
+    const chatbox = document.getElementById("chatbot-box");
+    chatbox.classList.toggle("hidden");
+});
