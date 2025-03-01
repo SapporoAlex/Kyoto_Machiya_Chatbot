8ec647609d4000113392271fe950f8c8956bdf72
Alexander McKinley <alexanderfromaustralia@gmail.com>
2025-03-01 14:01:40 +0900

    Delete static/chatBot.js

--- static/chatBot.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("chatbot-button").addEventListener("click", function () {
-    const chatbox = document.getElementById("chatbot-box");
-    chatbox.classList.toggle("hidden");
-});
